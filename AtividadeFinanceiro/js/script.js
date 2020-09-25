3cbb2717a9bfc61371d3598ba00613de528ad57a
lucascastrop <61791008+lucascastrop@users.noreply.github.com>
2020-09-24 22:20:50 -0300

    Add files via upload

--- AtividadeFinanceiro/js/script.js
+++ AtividadeFinanceiro/js/script.js
@@ -1,59 +1,75 @@
+
+
 //Restrições para a página de solicitação de empréstimos
-document.dados.salario.addEventListener('blur', validarSalario)
+document.emprestimo.salario.addEventListener('blur', validarSalario)
 function validarSalario() {
-  var salario = parseFloat(document.dados.salario.value)
+  var salario = parseFloat(document.emprestimo.salario.value)
   var resSal = document.querySelector('#idSalario~span')
 
   if(salario <= 0 || isNaN(salario)){
     resSal.innerHTML = "Insira uma quantia válida"
-    event.preventDefault()
+    return 1
   }else{
     resSal.innerHTML = ""
+    return 0
   }
 }
 
-document.dados.tempoemp.addEventListener('blur', validarTempo)
+document.emprestimo.tempoemp.addEventListener('blur', validarTempo)
 function validarTempo() {
-  var tempo = parseFloat(document.dados.tempoemp.value)
+  var tempo = parseFloat(document.emprestimo.tempoemp.value)
   var resTemp = document.querySelector('#idTempoEmp~span')
 
   if(tempo <= 0 || isNaN(tempo) || tempo > 100){
     resTemp.innerHTML = "Insira uma quantia válida"
-    event.preventDefault()
+    return 1
   }else{
     resTemp.innerHTML = ""
+    return 0
   }
 }
 
-document.dados.valor.addEventListener('blur', validarValor)
+document.emprestimo.valor.addEventListener('blur', validarValor)
 function validarValor() {
-  var valor = parseFloat(document.dados.valor.value)
+  var valor = parseFloat(document.emprestimo.valor.value)
   var resValor = document.querySelector('#idValor~span')
   if(valor <= 0 || isNaN(valor)){
     resValor.innerHTML = "Insira uma quantia válida"
-    event.preventDefault()
+    event.preventDefault('enviar')
+    return 1
   }else{
     resValor.innerHTML = ""
+    return 0
   }
 }
 
-document.dados.enviar.addEventListener('click', validarPedido)
+document.emprestimo.enviar.addEventListener('click', validarPedido)
 function validarPedido() {
-  var salario = parseFloat(document.dados.salario.value)
-  var tempo = parseFloat(document.dados.tempoemp.value)
-  var valor = parseFloat(document.dados.valor.value)
+  var valid = 0
+  valid += validarSalario()
+  valid += validarTempo()
+  valid += validarValor()
+
+  var salario = parseFloat(document.emprestimo.salario.value)
+  var tempo = parseFloat(document.emprestimo.tempoemp.value)
+  var valor = parseFloat(document.emprestimo.valor.value)
   var emprestimo = valor/salario
-    if(tempo < 3){
-      if(emprestimo <= 0.7){
-        alert("Pedido enviado para análise!")
-      }else{
-        alert("O valor do empréstimo não pode ser superior a 70% do seu salário")
-      }
-    }else if(tempo => 3){
-        if(emprestimo <= 1.5){
+  if(valid == 0){
+      if(tempo < 3){
+        if(emprestimo <= 0.7){
           alert("Pedido enviado para análise!")
         }else{
-          alert("O valor do empréstimo não pode ser superior a 150% do seu salário")
+          alert("O valor do empréstimo não pode ser superior a 70% do seu salário")
         }
+      }else if(tempo => 3){
+          if(emprestimo <= 1.5){
+            alert("Pedido enviado para análise!")
+          }else{
+            alert("O valor do empréstimo não pode ser superior a 150% do seu salário")
+          }
+      }
+    }else{
+        alert("Preencha todos os campos corretamente!")
     }
+
 }
